c7a08d24ba49c73e29599cccf33c0ae255d3fd4c
Arjun Senthil <arjunsenthil.dev@gmail.com>
2023-06-05 15:19:48 +0530

    added semantic-ui-react for easier ui dev

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
-import factory from '../ethereum/factory';
+import React, { Component } from "react";
+import { Button, Card } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
-    static async getInitialProps() {
-        const campaigns = await factory.methods.getDeployedCampaigns().call();
-        return { campaigns };
-    }
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+    return { campaigns };
+  }
 
-    render() {
-        return <div>{this.props.campaigns[0]}</div>
-    }
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campagins</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Open Campaigns</h3>
+        {this.renderCampaigns()}
+        <Button content="Create Campaign" icon="add circle" primary/>
+      </div>
+    );
+  }
 }
 
 export default CampaignIndex;
