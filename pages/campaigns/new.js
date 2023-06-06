1a8d67c4d05fe9aceede24622bb66f9ec68a655d
Arjun Senthil <arjunsenthil.dev@gmail.com>
2023-06-06 13:03:13 +0530

    added file handling now working on next-routes

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,21 +1,54 @@
 import React, { Component } from "react";
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Input, Message} from 'semantic-ui-react';
 import Layout from "../../components/Layout";
 import "semantic-ui-css/semantic.min.css";
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaginNew extends Component {
+
+    state = {
+        minimumContribution: '',
+        errorMessage: '',
+        loading: false
+    };
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+        
+        this.setState({loading: true});
+
+        try{
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+            .createCampaign(this.state.minimumContribution)
+            .send({
+                from: accounts[0]
+            });
+        } catch (err) {
+            this.setState({errorMessage: err.message});
+        }
+
+        this.setState({loading: false});
+    };
+
     render() {
         return (
             <Layout>
             <h3>Create a Campaign</h3>
             
-            <Form>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label>Minimum Contribution</label>
-                    <input />
+                    <Input 
+                        label="wei" 
+                        labelPosition="right" 
+                        value={this.state.minimumContribution} 
+                        onChange={event => this.setState({minimumContribution: event.target.value})}
+                    />
                 </Form.Field>
-
-                <Button primary>Create</Button>
+                <Message error header="Oops!" content={this.state.errorMessage} />
+                <Button loading={this.state.loading} primary>Create</Button>
             </Form>
 
             </Layout>
