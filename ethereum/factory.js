d489e4c400121a1770f4413fe25e34d808bdc0d5
Arjun Senthil <arjunsenthil.dev@gmail.com>
2023-06-06 18:29:50 +0530

    added campaign show for each address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x6c80b21FF07E999f1e3Ed277B1B93F196e0b2CEb'
+    '0x8eb7Cd9FE06Add554EbA9aA627199c69225Edb39'
 );
 
 export default instance;
