9c301efdbc797747484edf388f36d7b25fb3144b
Arjun Senthil <arjunsenthil.dev@gmail.com>
2023-06-06 18:57:21 +0530

    added campaign show features

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
-import { Card } from "semantic-ui-react";
+import { Card, Grid } from "semantic-ui-react";
 import campaignInstance from "../../ethereum/campaign";
-import web3 from '../../ethereum/web3';
+import web3 from "../../ethereum/web3";
+import ContributeForm from "../../components/ContributeForm";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -33,36 +34,50 @@ class CampaignShow extends Component {
         meta: "Address of Manager",
         description:
           "The manager created this campaign and can create requests to withdraw money.",
-        style: { overflowWrap: 'break-word' }
+        style: { overflowWrap: "break-word" },
       },
       {
         header: minimumContribution,
         meta: "Minimum Contribution (wei)",
-        description: "You must contribute atleast this much wei to become an approver.",
+        description:
+          "You must contribute atleast this much wei to become an approver.",
       },
       {
         header: requestsCount,
-        meta: 'Number of Requests',
-        description: "A request tries to withdraw money from the contract. Requests must be approved by the approvers."
+        meta: "Number of Requests",
+        description:
+          "A request tries to withdraw money from the contract. Requests must be approved by the approvers.",
       },
       {
         header: approversCount,
         meta: "Number of Approvers",
-        description: "Number of people who have already donated to this campaign."
+        description:
+          "Number of people who have already donated to this campaign.",
       },
       {
-        header: web3.utils.fromWei(balance, 'ether'),
-        meta: 'Campaign Balance (ether)',
-        description: 'Balance is how much money this campaign has left to spend'
-      }
-
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description:
+          "Balance is how much money this campaign has left to spend",
+      },
     ];
 
     return <Card.Group items={items} />;
   }
 
   render() {
-    return <Layout>{this.renderCards()}</Layout>;
+    return (
+      <Layout>
+        <h3>Campaign Show</h3>
+        <Grid>
+          <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+
+          <Grid.Column width={6}>
+            <ContributeForm />
+          </Grid.Column>
+        </Grid>
+      </Layout>
+    );
   }
 }
 
